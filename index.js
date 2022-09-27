f65883a22adf111a9c3a9f140e82e34e28e999c6
ali houssa <81270840+itsalivenger@users.noreply.github.com>
2022-09-28 00:06:51 +0100

    dzald

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const app = express();
 const port = 5050;
 
 app.use(cors({
-    origin: 'http://127.0.0.1:5500/index.html'
+    origin: 'https://itsalivenger.github.io/Crem-Dev/',
+    credentials: true
 }));
 
 app.use(bodyparser.json());
