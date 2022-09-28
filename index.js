ed8104408be539349afbdee92a74385ee3c85704
ali houssa <81270840+itsalivenger@users.noreply.github.com>
2022-09-28 01:08:56 +0100

    dzald

--- index.js
+++ index.js
@@ -12,27 +12,28 @@ app.use(cors({
 
 app.use(bodyparser.json());
 
-app.post('/', (req, res)=>{
-    console.log('post')
-    let { name, email, subject, message } = req.body;
-    const mailOptions = {
-        from: 'alihoussa16@gmail.com',
-        to: 'alihoussa16@gmail.com',
-        subject: subject,
-        text: `name: ${name} \n
-                email: ${email}\n
-            ${message}`
-    }
+app.get('/', (req, res)=>{
+    console.log('get')
+    // let { name, email, subject, message } = req.body;
+    // const mailOptions = {
+    //     from: 'alihoussa16@gmail.com',
+    //     to: 'alihoussa16@gmail.com',
+    //     subject: subject,
+    //     text: `name: ${name} \n
+    //             email: ${email}\n
+    //         ${message}`
+    // }
 
-    transporter.sendMail(mailOptions, (err, data)=>{
-        if(err){
-            console.log('error Occured f had blasa : ', err);
-            res.send({msg: 'there was an error'});
-        }else{
-            console.log('email sent successefully')
-            res.send({msg: 'email sent successefully'});
-        }
-    })
+    // transporter.sendMail(mailOptions, (err, data)=>{
+    //     if(err){
+    //         console.log('error Occured f had blasa : ', err);
+    //         res.send({msg: 'there was an error'});
+    //     }else{
+    //         console.log('email sent successefully')
+    //         res.send({msg: 'email sent successefully'});
+    //     }
+    // })
+    res.send({msg: 'noted'})
 })
 
 app.listen(port, ()=>{
